21b0e02131300661bc0962c4003c66f922330177
NiLuJe <ninuje@gmail.com>
2024-12-04 13:39:54 +0100

    Avec la gestion des phrases (très approximativement...)

--- programmes/make_pals_corpus.sh
+++ programmes/make_pals_corpus.sh
@@ -11,8 +11,10 @@ BASE_DIR="$(readlink -f "${SCRIPT_NAME%/*}/..")"
 # On préfère certains outils GNU sous macOS...
 if [ "$(uname -s)" == "Darwin" ] ; then
 	GREP_BIN="ggrep"
+	SED_BIN="gsed"
 else
 	GREP_BIN="grep"
+	SED_BIN="sed"
 fi
 
 # Validation basique du paramètre
@@ -35,4 +37,5 @@ OUTPUT_CTX_REL="contextes/${LANG}-*.txt"
 OUTPUT_CTX="${BASE_DIR}/${OUTPUT_CTX_REL}"
 
 # Tokenization
-cat ${OUTPUT_TXT} | ${GREP_BIN} -Po "\b[-\p{l}]+\b"
+# FIXME: C'est plutôt bof la gestion des phrases... (la répétition foireuse c'est pour laisser les middle name US tranquilles, mais ça laisse quand même passer quelques abréviations...)
+cat ${OUTPUT_TXT} | ${SED_BIN} -re 's/(\w{2,}\b)([.?!])/\1 %EoS%/g' | ${GREP_BIN} -Po "\b[-\p{l}]+\b" | sed "s/EoS//"
