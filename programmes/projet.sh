4268d3a319cabdc571cb02c063fef3e245db3d60
NiLuJe <ninuje@gmail.com>
2024-12-18 11:39:13 +0100

    Add stuff TODO

--- programmes/projet.sh
+++ programmes/projet.sh
@@ -11,6 +11,7 @@ SCRIPT_NAME="${BASH_SOURCE[0]}"
 BASE_DIR="$(readlink -f "${SCRIPT_NAME%/*}/..")"
 
 # TODO: Merge bigram
+# TODO: PALS
 # FIXME: Gestion des flexions et/ou mots composés (concordancier en particulier?)
 
 # On préfère certains outils GNU sous macOS...
