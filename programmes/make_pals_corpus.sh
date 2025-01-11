e524faa54b6fd8190d63c5b9dc896e988909f9eb
NiLuJe <ninuje@gmail.com>
2025-01-12 00:35:01 +0100

    Import des corpus PALS FR

--- programmes/make_pals_corpus.sh
+++ programmes/make_pals_corpus.sh
@@ -33,14 +33,17 @@ fi
 LANG="${LANG^^}"
 OUTPUT_TXT_REL="dumps-text/${LANG}-*.txt"
 OUTPUT_TXT="${BASE_DIR}/${OUTPUT_TXT_REL}"
+OUTPUT_TXT_PAL="${BASE_DIR}/pals/dumps-text-${LANG}.txt"
 OUTPUT_CTX_REL="contextes/${LANG}-*.txt"
 OUTPUT_CTX="${BASE_DIR}/${OUTPUT_CTX_REL}"
+OUTPUT_CTX_PAL="${BASE_DIR}/pals/contextes-${LANG}.txt"
 
 # Tokenization
 >&2 echo "Tokenization des dumps texte en ${LANG}"
-# FIXME: C'est plutôt bof la gestion des phrases... (la répétition foireuse c'est pour laisser les middle name US tranquilles, mais ça laisse quand même passer quelques abréviations...)
-# TODO: pymorphy2? udpipe?
-cat ${OUTPUT_TXT} | ${SED_BIN} -re 's/(\w{2,}\b)([.?!])/\1 %EoS%/g' | ${GREP_BIN} -Po "\b[-\p{l}]+\b" | sed "s/EoS//"
+# NOTE: C'est plutôt bof la gestion des phrases... (la répétition foireuse c'est pour laisser les middle name US tranquilles, mais ça laisse quand même passer quelques abréviations...)
+# TODO: pymorphy2? udpipe? Unitex?
+cat ${OUTPUT_TXT} | ${SED_BIN} -re 's/(\w{2,}\b)([.?!])/\1 %EoS%/g' | ${GREP_BIN} -Po "\b[-\p{l}]+\b" | sed "s/EoS//" > "${OUTPUT_TXT_PAL}"
 
 # Rebelotte pour les contextes
 >&2 echo "Tokenization des contextes en ${LANG}"
+cat ${OUTPUT_CTX} | ${SED_BIN} -re 's/(\w{2,}\b)([.?!])/\1 %EoS%/g' | ${GREP_BIN} -Po "\b[-\p{l}]+\b" | sed "s/EoS//" > "${OUTPUT_CTX_PAL}"
